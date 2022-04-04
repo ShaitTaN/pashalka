07c9df6f9197db632f354fbb51fbd8538204880f
Dmitriy <kdima18@mail.ru>
2022-04-05 01:49:49 +0300

    Fake menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,6 @@
 const roflBtn = document.querySelector('.rfl-btn')
+const menuItms = document.querySelectorAll('.menu__item')
+const menu = document.querySelector('.menu')
 
 const handleRofl = (e) => {
 	const btn = e.target
@@ -10,3 +12,21 @@ const handleRofl = (e) => {
 
 roflBtn.addEventListener('mouseenter', handleRofl)
 roflBtn.addEventListener('click', () => alert('Может быть картинка?'))
+
+const handleRotate = (e) => {
+	item = e.target
+	console.log(item.dataset.num)
+	switch(item.dataset.num){
+		case '1':
+			menu.style.transform = 'rotate(180deg)'
+			break;
+		case '2':
+			menu.style.transform = 'rotate(90deg)'
+			break;
+		case '3':
+			menu.style.transform = 'rotate(0deg)'
+			break;
+	}
+}
+
+menuItms.forEach(item => item.addEventListener('click', handleRotate))
