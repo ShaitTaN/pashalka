9806ac9968de3014b8454654b0c3a7cddb2412ce
Dmitriy <kdima18@mail.ru>
2022-04-04 17:45:50 +0300

    Add btn

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+const roflBtn = document.querySelector('.rfl-btn')
+
+const handleRofl = (e) => {
+	const btn = e.target
+
+	btn.style.position = 'absolute'
+	btn.style.top = `${Math.random()*80}%`
+	btn.style.left = `${Math.random()*80}%`
+}
+
+roflBtn.addEventListener('mouseenter', handleRofl)
+roflBtn.addEventListener('click', () => alert('Molodec'))
